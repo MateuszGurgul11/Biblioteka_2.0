06748ffbc6b3bbc28cc359bed9ab1a93f7601f03
Mateusz <mateusz.gurgul.2003@gmail.com>
2024-04-01 15:57:29 +0200

    Koniec szkieletu projektu

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -7,5 +8,6 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+csrf = CSRFProtect(app)
 
 from app import route, models
--- app/route.py
+++ app/route.py
@@ -3,7 +3,6 @@ from app import app, db
 from app.models import Author, Book, Borrowing
 
 @app.route("/")
-@app.route("/index")
 def homepage():
     books = Book.query.all()
     return render_template("index.html", title = "Biblioteka", books=books)
@@ -14,16 +13,42 @@ def add_book():
         name = request.form['name']
         page_number = request.form['page_number']
         author_name = request.form['author']
+        lastname = request.form['lastname']
+        age = request.form['age']
 
-        author = Author.query.filter_by(name = author_name).first()
+        author = Author.query.filter_by(name=author_name, lastname=lastname, age=age).first()
         if not author:
-            author = Author(name = author_name)
+            author = Author(name=author_name, lastname=lastname, age=age)
             db.session.add(author)
-            db.session.commit()
         
         new_book = Book(name = name, page_number = page_number, authors = [author])
         db.session.add(new_book)
         db.session.commit()
         return redirect(url_for("homepage"))
     return render_template("add_book.html")
+
+@app.route("/delete_book/<int:book_id>", methods=['POST', 'GET'])
+def delete_book(book_id):
+    if request.method == 'POST':
+        book = Book.query.get_or_404(book_id)
+        authors = list(book.authors)
+        db.session.delete(book)
+        db.session.commit()
+
+        for author in authors:
+            if not author.books:
+                db.session.delete(author)
+        
+        db.session.commit()
+
+        return redirect(url_for('homepage'))
+
+@app.route("/toggle_book/<int:book_id>", methods=['POST', 'GET'])
+def toggle_book(book_id):
+    if request.method == 'POST':
+        book = Book.query.get_or_404(book_id)
+        book.is_borrowed = not book.is_borrowed
+        db.session.commit()
+        return redirect(url_for("homepage"))
+
 
